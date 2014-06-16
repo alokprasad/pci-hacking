68d2eeadf83c366f90da44156c55991441ce1e8b
Alok.prasad7@gmail.com <Alok.prasad7@gmail.com@176ccadd-d469-b2b0-c757-89d10f65a467>
2014-06-16 20:35:01 +0000

    changed copyrighted material

--- trunk/virtual_pcinet/virtual_pci.c
+++ trunk/virtual_pcinet/virtual_pci.c
@@ -1,34 +1,4 @@
 /*
- * Copyright (c) 2006-2012 Xsigo Systems Inc.  All rights reserved.
- *
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
- * OpenIB.org BSD license below:
- *
- *     Redistribution and use in source and binary forms, with or
- *     without modification, are permitted provided that the following
- *     conditions are met:
- *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
- *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
  */
 
 #include <linux/version.h>
@@ -41,11 +11,7 @@
 #include <linux/kobject.h>
 #include <linux/delay.h>
 #include <linux/device.h>
-#include <linux/netdevice.h>
-#include <linux/rtnetlink.h>
 #include <linux/proc_fs.h>
-#include <linux/spinlock.h>
-#include <linux/spinlock_types.h>
 #include <linux/types.h>
 #include <linux/kobject.h>
 #include <linux/slab.h>
@@ -57,37 +23,29 @@
 #include <linux/log2.h>
 #include <linux/mutex.h>
 #include <linux/version.h>
-#include <linux/idr.h>
 #include <linux/netdevice.h>
-#include <linux/tcp.h>
-#include <linux/workqueue.h>
-#include <rdma/ib_verbs.h>
 
-//#include "xs_compat.h"
-//#include "xscore.h"
 
-int xscore_vpci_enable = 1;
-module_param(xscore_vpci_enable, int, 0644);
 
-#define PCI_VENDOR_ID_XSIGO		0x199d
-#define PCI_DEVICE_ID_XSIGO_VNIC	0x8209
+#define PCI_VENDOR_ID_XTREME		0x8086
+#define PCI_DEVICE_ID_XTREME_VNIC	0x1000
 
 static struct pci_bus *vbus;
 static struct pci_sysdata *sysdata;
 
-static DEFINE_PCI_DEVICE_TABLE(xs_vpci_dev_table) = {
-	{PCI_DEVICE(PCI_VENDOR_ID_XSIGO, PCI_DEVICE_ID_XSIGO_VNIC)},
+static DEFINE_PCI_DEVICE_TABLE(xt_vpci_dev_table) = {
+	{PCI_DEVICE(PCI_VENDOR_ID_XTREME, PCI_DEVICE_ID_XTREME_VNIC)},
 	{0}
 };
 
-MODULE_DEVICE_TABLE(pci, xs_vpci_dev_table);
+MODULE_DEVICE_TABLE(pci, xt_vpci_dev_table);
 
-int xs_vpci_read(struct pci_bus *bus, unsigned int devfn, int where,
+int xt_vpci_read(struct pci_bus *bus, unsigned int devfn, int where,
 		 int size, u32 *val)
 {
 	switch (where) {
 	case PCI_VENDOR_ID:
-		*val = PCI_VENDOR_ID_XSIGO | PCI_DEVICE_ID_XSIGO_VNIC << 16;
+		*val = PCI_VENDOR_ID_XTREME | PCI_DEVICE_ID_XTREME_VNIC << 16;
 		/* our id */
 		break;
 	case PCI_COMMAND:
@@ -119,7 +77,7 @@ int xs_vpci_read(struct pci_bus *bus, unsigned int devfn, int where,
 	return 0;
 }
 
-int xs_vpci_write(struct pci_bus *bus, unsigned int devfn, int where,
+int xt_vpci_write(struct pci_bus *bus, unsigned int devfn, int where,
 		  int size, u32 val)
 {
 	switch (where) {
@@ -134,12 +92,12 @@ int xs_vpci_write(struct pci_bus *bus, unsigned int devfn, int where,
 	return 0;
 }
 
-struct pci_ops xs_vpci_ops = {
-	.read = xs_vpci_read,
-	.write = xs_vpci_write
+struct pci_ops xt_vpci_ops = {
+	.read = xt_vpci_read,
+	.write = xt_vpci_write
 };
 
-struct pci_dev *xs_vpci_prep_vnic(struct net_device *netdev, char *vnic_name,
+struct pci_dev *xt_vpci_prep_vnic(struct net_device *netdev, char *vnic_name,
 				  int devn)
 {
 	struct pci_dev *pcidev = NULL;
@@ -161,16 +119,16 @@ struct pci_dev *xs_vpci_prep_vnic(struct net_device *netdev, char *vnic_name,
 	SET_NETDEV_DEV(netdev, &pcidev->dev);
 	return pcidev;
 }
-EXPORT_SYMBOL(xs_vpci_prep_vnic);
+EXPORT_SYMBOL(xt_vpci_prep_vnic);
 
-void *xs_vpci_add_vnic(char *vnic_name, int devn)
+void *xt_vpci_add_vnic(char *vnic_name, int devn)
 {
 	struct pci_dev *pcidev;
 	struct net_device *netdev;
 	int ret;
 	//ALOK
 	netdev = alloc_netdev(1, "xsvnic%d", ether_setup);
-	xs_vpci_prep_vnic(netdev,"XSVNIC",0);
+	xt_vpci_prep_vnic(netdev,"XSVNIC",0);
 	if (vbus == NULL)
 		return NULL;
 	pcidev = pci_scan_single_device(vbus, devn);
@@ -202,9 +160,9 @@ void *xs_vpci_add_vnic(char *vnic_name, int devn)
 	}
 	return pcidev;
 }
-EXPORT_SYMBOL(xs_vpci_add_vnic);
+EXPORT_SYMBOL(xt_vpci_add_vnic);
 
-void xs_vpci_remove_vnic(struct net_device *netdev, void *hndl)
+void xt_vpci_remove_vnic(struct net_device *netdev, void *hndl)
 {
 	struct pci_dev *pcidev = hndl;
 
@@ -217,19 +175,19 @@ void xs_vpci_remove_vnic(struct net_device *netdev, void *hndl)
 	pci_remove_bus_device(pcidev);
 	pci_dev_put(pcidev);
 }
-EXPORT_SYMBOL(xs_vpci_remove_vnic);
+EXPORT_SYMBOL(xt_vpci_remove_vnic);
 
-void xs_vpci_vdev_remove(struct pci_dev *dev)
+void xt_vpci_vdev_remove(struct pci_dev *dev)
 {
 }
 
-static struct pci_driver xs_vpci_vdev_driver = {
-	.name = "Xsigo-Virtual-NIC",
-	.id_table = xs_vpci_dev_table,
-	.remove = xs_vpci_vdev_remove
+static struct pci_driver xt_vpci_vdev_driver = {
+	.name = "Xtreme-Virtual-NIC",
+	.id_table = xt_vpci_dev_table,
+	.remove = xt_vpci_vdev_remove
 };
 
-int xs_vpci_bus_init(void)
+int xt_vpci_bus_init(void)
 {
 	int i = 2;
 
@@ -238,7 +196,7 @@ int xs_vpci_bus_init(void)
 
 	sysdata = kzalloc(sizeof(void *), GFP_KERNEL);
 	while (i > 0) {
-		vbus = pci_scan_bus_parented(NULL, i, &xs_vpci_ops, sysdata);
+		vbus = pci_scan_bus_parented(NULL, i, &xt_vpci_ops, sysdata);
 		if (vbus != NULL)
 			break;
 		memset(sysdata, 0, sizeof(void *));
@@ -248,20 +206,20 @@ int xs_vpci_bus_init(void)
 		kfree(sysdata);
 		return -EINVAL;
 	}
-	if (pci_register_driver(&xs_vpci_vdev_driver) < 0) {
+	if (pci_register_driver(&xt_vpci_vdev_driver) < 0) {
 		pci_remove_bus(vbus);
 		vbus = NULL;
 		return -EINVAL;
 	}
 	printk("here");
-	xs_vpci_add_vnic("xsvnic",0);
+	xt_vpci_add_vnic("xsvnic",0);
 	return 0;
 }
 
-void xs_vpci_bus_remove(void)
+void xt_vpci_bus_remove(void)
 {
 	if (vbus) {
-		pci_unregister_driver(&xs_vpci_vdev_driver);
+		pci_unregister_driver(&xt_vpci_vdev_driver);
 		device_unregister(vbus->bridge);
 		pci_remove_bus(vbus);
 		kfree(sysdata);
@@ -273,7 +231,7 @@ void xs_vpci_bus_remove(void)
 static int __init pci_init(void)
 {
 	printk( "module loaded");
-	xs_vpci_bus_init();
+	xt_vpci_bus_init();
 	return 0;
 }
 
